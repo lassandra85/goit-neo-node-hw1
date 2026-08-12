bcb9037388745f9488419bdb500e9cdb7ceacb02
lassandra85 <lassandra85@gmail.com>
2026-08-12 12:16:58 +0200

    new

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
 import os from 'node:os';
+import process from 'node:process';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url'
+
+const fullPath = '/home/user/documents/report.txt';
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
+console.log('Директорія:', path.dirname(fullPath));
+console.log('Назва файлу:', path.basename(fullPath));
+console.log('Розширення:', path.extname(fullPath));
+
 
 console.log('Платформа:', os.platform());
 console.log('Архітектура:', os.arch());
@@ -6,3 +19,14 @@ console.log('Домашня тека користувача:', os.homedir());
 console.log('Кількість процесорів:', os.cpus().length);
 console.log('Загальний обсяг пам’яті (МБ):', Math.round(os.totalmem() / 1024 / 1024));
 console.log('Тимчасова директорія:', os.tmpdir());
+
+
+console.log(process.argv);
+console.log(process.version);
+console.log('Поточна робоча тека:', process.cwd());
+
+console.log('Поточний файл', __filename)
+console.log('Поточна тека', __dirname)
+
+
+
